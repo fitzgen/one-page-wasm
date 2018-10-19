a831ede9bc06e3d5480de114f50bc971be640fb4
Nick Fitzgerald <fitzgen@gmail.com>
2018-10-19 08:50:20 -0700

    Create shiny colors example!

--- entries/example/src/lib.rs
+++ entries/example/src/lib.rs
@@ -2,16 +2,30 @@ extern crate cfg_if;
 extern crate wasm_bindgen;
 
 mod utils;
-
-use cfg_if::cfg_if;
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+static mut TIME: u32 = 0;
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, example!");
+pub fn frame(frame_buffer: &mut [u8]) {
+    utils::set_panic_hook();
+
+    let time = unsafe {
+        let t = TIME;
+        TIME += 1;
+        t
+    };
+
+    for (y, row) in frame_buffer.chunks_mut(256 * 4).enumerate() {
+        for (x, chunk) in row.chunks_mut(4).enumerate() {
+            assert!(chunk.len() == 4);
+            let r = ( ( f64::from(time)  / 100.0 ).sin() * 128.0 + 128.0 ) as u8;
+            let g = ( ( f64::from(time)  / 10.0 ).cos() * 128.0 + 128.0 ) as u8;
+            let b = ( ( f64::from(time + x as u32 + y as u32)  / 50.0 ).cos() * 128.0 + 128.0 ) as u8;
+            chunk[0] = r;
+            chunk[1] = g;
+            chunk[2] = b;
+            chunk[3] = 255;
+        }
+    }
 }
